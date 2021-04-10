2a938f666f8171e498c5977469233fd44404c197
kalibianw <kalibianw@gmail.com>
2021-04-11 05:30:41 +0900

    Changing type of y_data float to int.

--- generate_to_torch/prac.py
+++ generate_to_torch/prac.py
@@ -6,10 +6,10 @@ BATCH_SIZE = 32
 
 dm = DataModule(batch_size=BATCH_SIZE, shuffle=True)
 nploader = np.load("splited_Pneumonia_all_true_false_split_1_(300, 300).npz")
-train_x_data = nploader["train_x_data"]
+train_x_data = nploader["train_x_data"] / 255.0
 train_x_data = np.expand_dims(train_x_data, axis=1)
 print(np.shape(train_x_data))
-train_y_data = nploader["train_y_data"]
+train_y_data = nploader["train_y_data"].astype(np.long)
 
 for x_data, y_data in zip(train_x_data, train_y_data):
     print(x_data)
