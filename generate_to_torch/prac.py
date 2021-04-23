131122ea16895d36f81dfd63e9d061e09dd102b1
Jongbin Chae <kalibianw@gmail.com>
2021-04-24 02:34:09 +0900

    2021_04_24_02:34

--- generate_to_torch/prac.py
+++ generate_to_torch/prac.py
@@ -1,30 +1,10 @@
-from utils import DataModule
-from tensorflow.keras.utils import to_categorical
-import numpy as np
-import cv2
+import os
 
-BATCH_SIZE = 32
 
-dm = DataModule(batch_size=BATCH_SIZE, shuffle=True)
-nploader = np.load("splited_Pneumonia_all_true_false_split_1_(300, 300).npz")
-train_x_data = nploader["train_x_data"] / 255.0
-train_x_data = np.expand_dims(train_x_data, axis=1)
-print(np.shape(train_x_data))
-train_y_data = nploader["train_y_data"].astype(np.long)
+run_file_paths = [
+    "splited_Pneumonia_all_true_false_split_1_(300, 300).py",
+    "splited_Pneumonia_all_true_false_split_1_0_(300, 300).py"
+]
 
-for x_data, y_data in zip(train_x_data, train_y_data):
-    print(x_data)
-    print(x_data.max(), x_data.min())
-    print(y_data)
-    print(np.shape(x_data))
-    print(type(x_data[0, 0, 0]))
-    print(np.shape(y_data))
-    print(type(y_data))
-
-    break
-
-train_loader = dm.np_to_dataloader(train_x_data, train_y_data)
-for batch_idx, (image, label) in enumerate(train_loader):
-    print(label)
-
-    break
+for current_index in range(len(run_file_paths)):
+    os.system(f"python -m {run_file_paths[current_index]}")
---
+++ generate_to_torch/splited_Pneumonia_all_true_false_split_1_0_(300, 300).py
@@ -0,0 +1,122 @@
+from utils import DataModule, NeuralNetwork, TrainModule
+from torch.utils.tensorboard import SummaryWriter
+from torchinfo import summary
+from torch import optim
+import torch.nn as nn
+import torch
+import numpy as np
+import time
+import sys
+import os
+
+BATCH_SIZE = 32
+EPOCHS = 1000
+MODEL_PATH = "splited_Pneumonia_all_true_false_split_1_0_(300, 300).pt"
+LOCAL_TIME = time.localtime()
+LOG_FOLDER_PATH = f"./torch_logs/" \
+                  f"{LOCAL_TIME[0]}_" \
+                  f"{LOCAL_TIME[1]}_" \
+                  f"{LOCAL_TIME[2]}_" \
+                  f"{LOCAL_TIME[3]}_" \
+                  f"{LOCAL_TIME[4]}_" \
+                  f"{LOCAL_TIME[5]}"
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+LEARNRING_RATE = 1e-3
+REDUCE_LR_PATIENCE = 5
+REDUCE_LR_RATE = 0.6
+LOG_INTERVAL = 32
+EARLY_STOPPING_CNT = 31
+
+dm = DataModule(batch_size=BATCH_SIZE, shuffle=True)
+nploader = np.load("splited_Pneumonia_all_true_false_split_1_0_(300, 300).npz")
+for key in nploader:
+    print(key)
+
+train_x_data = nploader["train_x_data"] / 255.0
+train_x_data = np.expand_dims(train_x_data, axis=1)
+print(np.shape(train_x_data))
+train_y_data = nploader["train_y_data"]
+train_loader = dm.np_to_dataloader(train_x_data, train_y_data)
+del train_x_data
+del train_y_data
+
+valid_x_data = nploader["valid_x_data"] / 255.0
+valid_x_data = np.expand_dims(valid_x_data, axis=1)
+print(np.shape(valid_x_data))
+valid_y_data = nploader["valid_y_data"]
+valid_loader = dm.np_to_dataloader(valid_x_data, valid_y_data)
+del valid_x_data
+del valid_y_data
+
+test_x_data = nploader["test_x_data"] / 255.0
+test_x_data = np.expand_dims(test_x_data, axis=1)
+print(np.shape(test_x_data))
+test_y_data = nploader["test_y_data"]
+test_loader = dm.np_to_dataloader(test_x_data, test_y_data)
+del test_x_data
+del test_y_data
+
+neural_network = NeuralNetwork()
+model = neural_network.to(DEVICE)
+summary(model, input_size=(32, 1, 300, 300))
+
+optimizer = optim.Adam(model.parameters(), lr=LEARNRING_RATE)
+loss = nn.CrossEntropyLoss()
+tm = TrainModule(
+    device=DEVICE,
+    optimizer=optimizer,
+    loss=loss,
+    bach_size=BATCH_SIZE,
+    reduce_lr_rate=REDUCE_LR_RATE,
+    reduce_lr_patience=REDUCE_LR_PATIENCE
+)
+
+os.makedirs(LOG_FOLDER_PATH)
+
+writer = SummaryWriter(log_dir=LOG_FOLDER_PATH)
+current_time = time.time()
+test_best_loss = sys.maxsize    # for Early stopping
+valid_best_loss = sys.maxsize   # for Reduce learning rate
+not_improve_cnt = 0
+for Epoch in range(0, EPOCHS):
+    train_acc, train_loss, valid_acc, valid_loss, current_lr = tm.training(
+        model,
+        train_loader=train_loader,
+        valid_loader=valid_loader,
+        log_interval=LOG_INTERVAL,
+        best_loss=valid_best_loss
+    )
+    print("\n[EPOCH: {}], \tTrain Loss: {:.4f}, \tTrain Accuracy: {:.2f}%\tLearning Rate: {}".format(Epoch,
+                                                                                                     train_loss,
+                                                                                                     train_acc,
+                                                                                                     current_lr))
+    print("[EPOCH: {}], \tValid Loss: {:.4f}, \tValid Accuracy: {:.2f}%".format(Epoch, valid_loss, valid_acc))
+
+    test_acc, test_loss = tm.evaluate(model, test_loader)
+    print("[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f}%\n".format(Epoch, test_loss, test_acc))
+
+    writer.add_scalar("Loss/train", train_loss, Epoch)
+    writer.add_scalar("Loss/valid", valid_loss, Epoch)
+    writer.add_scalar("Loss/test", test_loss, Epoch)
+    writer.add_scalar("Accuracy/train", train_acc, Epoch)
+    writer.add_scalar("Accuracy/valid", valid_acc, Epoch)
+    writer.add_scalar("Accuracy/test", test_acc, Epoch)
+    writer.add_text(tag="Model description", text_string="Swish test")
+
+    if test_loss < test_best_loss:
+        torch.save(model.state_dict(), MODEL_PATH)
+        test_best_loss = test_loss
+        not_improve_cnt = 0
+    if test_loss > test_best_loss:
+        if not_improve_cnt > EARLY_STOPPING_CNT:
+            break
+        not_improve_cnt += 1
+    print(f"Early stopping non_iprove_cnt: {not_improve_cnt}")
+    print(f"Test best loss: {test_best_loss}")
+    print(f"Valid best loss: {valid_best_loss}")
+
+    if valid_loss < valid_best_loss:
+        valid_best_loss = valid_loss
+
+print(time.time() - current_time)
