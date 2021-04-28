2e097191e4531876c34286673b66955655bd25bd
kalibianw <kalibianw@gmail.com>
2021-04-28 16:25:31 +0900

    2021_04_28_16:25

--- generate_to_torch/prac.py
+++ generate_to_torch/prac.py
@@ -1,6 +1,7 @@
 from utils import NeuralNetwork, DataModule, TestModule
 import torch
 import numpy as np
+from torchsummary import summary
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -11,11 +12,13 @@ test_x_data = np.expand_dims(test_x_data, axis=1)
 dm = DataModule(32, shuffle=True)
 test_loader = dm.np_to_dataloader(test_x_data, test_y_data)
 
-MODEL_PATH = "splited_Pneumonia_all_true_false_split_1_0_(300, 300)_relu.pt"
+MODEL_PATH = "splited_Pneumonia_all_true_false_split_1_0_(300, 300)_combined.pt"
 network = NeuralNetwork()
 
-model = network.to(DEVICE)
-model.load_state_dict(torch.load(MODEL_PATH))
+# model = network.to(DEVICE)
+# model.load_state_dict(torch.load(MODEL_PATH))
+model = torch.load(MODEL_PATH)
+summary(model, input_size=(1, 300, 300), batch_size=32)
 
 model.eval()
 
